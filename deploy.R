493d0d9d236e7e261d1e3e49cdf3215afffe308d
liuzan-XMU <62529552+liuzan-info@users.noreply.github.com>
2023-07-21 01:18:40 +0800

    Add githubPAT

--- deploy.R
+++ deploy.R
@@ -1,30 +1,6 @@
 .libPaths("/usr/local/lib/R/site-library")
 cat("loading packages from:", paste("\n - ", .libPaths(), collapse = ""), "\n\n")
 
-# set up Repositories
-setRepositories(ind = c(1,2,3,4,5))
-
-# manually install some packages
-if (!require("BiocManager", quietly = TRUE)){install.packages("BiocManager")}
-
-BiocManager::install(c("pcaMethods", "impute"))
-
-install.packages(c("shiny","shinyjs","shinyBS","shinyWidgets","ggplot2","ggrepel","plotly", "colourpicker","ggseqlogo","pheatmap","survminer","survival","zip","stringr","dplyr","DT","png", "svglite","ggplotify","bslib","qpdf", "rrcovNA", "e1071", "heatmaply"))
-
-install.packages('devtools')
-require(devtools)
-
-install_github('evocellnet/ksea', force = TRUE)
-install_github('omarwagih/rmotifx', force = TRUE)
-install_github('ecnuzdd/PhosMap', force = TRUE)
-
-# use renv to detect and install required packages.
-if (file.exists("renv.lock")) {
-  renv::restore(prompt = FALSE)
-} else {
-  renv::hydrate()
-}
-
 # set up some helper functions for fetching environment variables
 defined <- function(name) {
   !is.null(Sys.getenv(name)) && Sys.getenv(name) != ""
@@ -41,6 +17,33 @@ optional <- function(name) {
   }
   Sys.getenv(name)
 }
+
+# set up Repositories
+setRepositories(ind = c(1,2,3,4,5))
+
+# manually install some packages
+if (!require("BiocManager", quietly = TRUE)){install.packages("BiocManager")}
+
+BiocManager::install(c("pcaMethods", "impute"))
+
+install.packages(c("shiny","shinyjs","shinyBS","shinyWidgets","ggplot2","ggrepel","plotly", "colourpicker","ggseqlogo","pheatmap","survminer","survival","zip","stringr","dplyr","DT","png", "svglite","ggplotify","bslib","qpdf", "rrcovNA", "e1071", "heatmaply"))
+
+install.packages('devtools')
+require(devtools)
+
+githubPAT <- optional("INPUT_GITHUBPAT")
+Sys.setenv(GITHUB_PAT = githubPAT)
+install_github('evocellnet/ksea', force = TRUE)
+install_github('omarwagih/rmotifx', force = TRUE)
+install_github('ecnuzdd/PhosMap', force = TRUE)
+
+# use renv to detect and install required packages.
+if (file.exists("renv.lock")) {
+  renv::restore(prompt = FALSE)
+} else {
+  renv::hydrate()
+}
+
 
 # resolve app dir
 # Note that we are likely already executing from the app dir, as
